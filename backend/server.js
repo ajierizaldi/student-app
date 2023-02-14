9b79458bfb1dbbd4f60aed08014706f92f32bf3d
ajierizaldi <ajiemuhammad2@gmail.com>
2023-02-14 12:35:31 +0700

    update backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', router)
+app.use('/api', router)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 
 const todo = require('./todo');
+const user = require('./user');
+
+router.use(cors());
 
 router.get('/check-health', (req, res) => {
     res.send('Hello World!');
